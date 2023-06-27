c83964651e87b93ad727a9a558fb7b4a212abad8
jaspreetScripts <119395932+jaspreetScripts@users.noreply.github.com>
2023-06-27 16:56:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+  function delayHello() {
+        return new Promise((resolve) => {
+          setTimeout(() => {
+            resolve('Hello, world!');
+          }, 1000);
+        });
+      }
+
+      const outputElement = document.getElementById('output');
+
+      delayHello().then((message) => {
+        outputElement.textContent = message;
+      });
